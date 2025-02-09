12b49b0e842bf8fcb26c2f8f0ef3a07728ac733b
Brown-Sage <luckytak60@gmail.com>
2025-02-10 00:20:05 +0530

    Created the bottom part

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import "../styles/Home.css";
 import Timer from "../components/Timer";
 import "../styles/Timer.css";
 import { MdOutlineMenuOpen } from "react-icons/md";
+import Menu from "../components/Menu";
 
 function Home() {
     const navigate = useNavigate();
@@ -11,7 +12,7 @@ function Home() {
         <div className="container">
             <div className="MainHead">
                 <div className="header-left">
-                    <MdOutlineMenuOpen />
+                    <button><MdOutlineMenuOpen color="white" fontSize={30} /></button>
                 </div>
                 <div className="header-right">
                     <button onClick={() => navigate("/login")}>Log In</button>
@@ -20,12 +21,14 @@ function Home() {
             </div>
             <div className="Maintimer">
                 <div className="Options">
-                    <button      className="child1">Focus</button>
+                    <button className="child1">Focus</button>
                     <button className="child1">Break</button>
                     <button className="child1">Long Break</button>
                 </div>
                 <Timer />
+                
             </div>
+            <Menu />
         </div>
     );
 }
