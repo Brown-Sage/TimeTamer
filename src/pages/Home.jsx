8d7a4080518e9aa7e5faef2acda9ac389846637e
Brown-Sage <luckytak60@gmail.com>
2025-02-12 23:05:54 +0530

    Home:Removed the buttons from Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,13 +10,6 @@ import "../styles/SpotifyFrame.css";
 function Home() {
     const navigate = useNavigate();
 
-    const handleBreak = () => {
-        // Call the break function exposed by Timer
-        if (window.handleBreakTimer) {
-            window.handleBreakTimer();
-        }
-    };
-
     return (
         <div className="container">
             <div className="MainHead">
@@ -32,11 +25,7 @@ function Home() {
                 <SpotifyFrame trackUri="7ouMYWpwJ422jRcDASZB7P" />
             </div>
             <div className="Maintimer">
-                <div className="Options">
-                    <button onClick = {Timer} className="child1">Focus</button>
-                    <button onClick={handleBreak} className="child1">Break</button>
-                    <button className="child1">Long Break</button>
-                </div>
+                
                 <Timer />
             </div>
             <Menu />
