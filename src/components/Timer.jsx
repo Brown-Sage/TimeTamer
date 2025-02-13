3477363f34016b282d7da22bd2e4acf141845c64
Brown-Sage <luckytak60@gmail.com>
2025-02-14 01:02:48 +0530

    Timer: Implementing Focus,Long Break and Break feature

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -22,6 +22,20 @@ function Timer() {
         setSeconds(0);
     };
 
+    const handleFocus = () => {
+        setIsRunning(false); // Start the timer when Focus is clicked
+        setHours(0);
+        setMinutes(50); // Reset to focus time (50 minutes)
+        setSeconds(0);
+    };
+
+    const handleLongBreak = () => {
+        setIsRunning(false);
+        setHours(0);
+        setMinutes(20); // Set long break time (20 minutes)
+        setSeconds(0);
+    };
+
     useEffect(() => {
         let interval;
         if (isRunning) {
@@ -53,14 +67,13 @@ function Timer() {
         return () => clearInterval(interval);
     }, [isRunning, minutes, seconds, hours]);
 
-    // Expose handleBreak to parent component
-    useEffect(() => {
-        // Make handleBreak available to parent
-        window.handleBreakTimer = handleBreak;
-    }, []);
-
     return (
         <>
+            <div className="Options">
+                <button onClick={handleFocus} className="child1">Focus</button>
+                <button onClick={handleBreak} className="child1">Break</button>
+                <button onClick={handleLongBreak} className="child1">Long Break</button>
+            </div>
             <div id="timer">{updateData()}</div>
             <button id="startStopBtn" onClick={startStop}>
                 {isRunning ? "Stop" : "Start"}
